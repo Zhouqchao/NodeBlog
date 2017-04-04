d63c8fc41e1c1675bf56f0d0380c5e7d13440ae9
Tom_chao <zhouqctech@gmail.com>
2017-04-04 22:45:20 +0800

    blog and admin layout

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -10,10 +10,22 @@ module.exports = function (app) {
 router.get('/', function (req, res, next) {
   Article.find(function (err, articles) {
     if (err) return next(err);
-    res.render('index', {
-      title: 'Generator-Express MVC',
+    res.render('blog/index', {
+      title: 'Node Blog',
       articles: articles,
       pretty:true
     });
   });
+});
+router.get('/about', function (req, res, next) {
+    res.render('blog/index', {
+      title: 'about me',
+      pretty:true
+    });
+});
+router.get('/contact', function (req, res, next) {
+    res.render('blog/index', {
+      title: 'contact me',
+      pretty:true
+    });
 });
